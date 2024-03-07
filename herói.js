490fd471a83552329c2422c8b15e5e18e0d6bf23
Yasmin <162188169+yasminsan009@users.noreply.github.com>
2024-03-07 18:48:34 -0300

    Update herói.js
    Atualizado

--- herói.js
+++ herói.js
@@ -1,34 +1,34 @@
-//Desafio - Classificação do herói
-
 console.log("Perfil do Herói")
 console.log("  ")
 console.log("Nome do herói: Mclaren")
 
 let XP = 7900
+let ss = "Status:"
+
 console.log("Quantidade de XP: " + XP)
 
 if( XP < 1001){
-	console.log("Status: Ferro")
+	console.log(ss + " Ferro")
 }else if(XP < 2001){
-	console.log("Status: Bronze")
+	console.log(ss +" Bronze")
 }else if(XP < 3001){
-	console.log("Status: Prata")
+	console.log(ss + " Prata")
 }else if(XP < 4001){
-	console.log("Status: Ouro")
+	console.log(ss + " Ouro")
 }else if(XP < 6001){
-	console.log("Status: Platina")
+	console.log(ss + " Platina")
 }else if(XP < 7001){
-	console.log("Status: Mestre")
+	console.log(ss + " Mestre")
 }else if(XP < 8001){
-	console.log("Status: Ascendente")
+	console.log(ss + " Ascendente")
 }else if(XP < 9001){
-	console.log("Status: Mestre")
+	console.log(ss + " Mestre")
 }else if(XP < 10001){
-	console.log("Status: Ascendente")
+	console.log(ss + " Ascendente")
 }else{
-    console.log("Status: Radiante")
+    console.log(ss + " Radiante")
 }
 
+
 console.log("  ")
-console.log("O nosso herói Mclaren está próximo a subir de status") 
-console.log("Falta apenas 101 pontos, continue jogando para alcançar nível Mestre! ")
+console.log("O nosso herói Mclaren alcançou o nível Ascendente!" ) 
